05a548d427815b1c77a07d1a9146cdba5a37750c
WillHitch-ai <williamhitchcock89@gmail.com>
2026-08-27 16:34:25 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const C='es-books-v4';const A=['./','./index.html','./manifest.json','./icon.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET'||!e.request.url.startsWith(self.location.origin))return;e.respondWith(fetch(e.request).then(r=>{const cp=r.clone();caches.open(C).then(c=>c.put(e.request,cp));return r}).catch(()=>caches.match(e.request)))});
